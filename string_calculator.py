1cb4835ccf8a5bb8d159abf487a7f6b823ba2542
Sumanta Mandal <sumanta.m@celestialsys.com>
2025-01-28 00:33:04 +0530

    3rd commit for multiple strings

--- string_calculator.py
+++ string_calculator.py
@@ -1,4 +1,4 @@
 def add(numbers: str) -> int:
     if not numbers:
         return 0
-    return int(numbers)
+    return sum([int(num) for num in numbers.split(',')])
--- string_calculator_test.py
+++ string_calculator_test.py
@@ -6,3 +6,9 @@ def test_empty_string_returns_zero():
 
 def test_single_number_returns_value():
     assert add("1") == 1
+
+def test_two_numbers_return_sum():
+    assert add("1,2") == 3
+
+def test_multiple_numbers_return_sum():
+    assert add("1,2,3,4") == 10
