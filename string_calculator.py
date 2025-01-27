828b87a99c2e485ec9181283ef1a97f26ec33ce4
Sumanta Mandal <sumanta.m@celestialsys.com>
2025-01-28 00:39:05 +0530

    4th commit for new line as delimiter

--- string_calculator.py
+++ string_calculator.py
@@ -1,4 +1,5 @@
 def add(numbers: str) -> int:
     if not numbers:
         return 0
+    numbers = numbers.replace("\n", ",")
     return sum([int(num) for num in numbers.split(',')])
--- string_calculator_test.py
+++ string_calculator_test.py
@@ -12,3 +12,9 @@ def test_two_numbers_return_sum():
 
 def test_multiple_numbers_return_sum():
     assert add("1,2,3,4") == 10
+
+def test_newline_as_delimiter_one():
+    assert add("1\n2,3") == 6
+
+def test_newline_as_delimiter_two():
+    assert add("5\n7,3") == 15
